ed0dda065133c4d14f4305c30e5d9fd7f3e79ff2
ravitewatia <tewatia35@gmail.com>
2024-12-27 16:29:14 +0530

    1.2 setting up

---
+++ src/main/java/com/SmartEnterprise/ravi/introductionweek1/introductionToSpringBoot/IntroductionToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.SmartEnterprise.ravi.introductionweek1.introductionToSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SmartEnterprise/ravi/introductionweek1/introductionToSpringBoot/IntroductionToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartEnterprise.ravi.introductionweek1.introductionToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
